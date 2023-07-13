ee01c516cd24e59dcfc89d2398c348e18c813f87
saeed <saeed.zakeri1364@gmail.com>
2023-07-13 17:19:39 -0400

    Rent-House-Angular-App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RegisterComponent} from './component/register/register.component'
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/register/register.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup | undefined;
+  submitted = false;
+  dataLoaded=false;
+  constructor(
+    private  formBuilder:FormBuilder,
+    private authService:AuthService,
+    private toasterService:ToastrService,
+  ) { }
+
+  get f() { return this.registerForm.controls; }
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+  createLoginForm(){
+    this.registerForm=this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+  register(){
+    if(this.registerForm.valid){
+      let registerModel =Object.assign({},this.registerForm.value)
+        this.authService.register(registerModel).subscribe(response=>{
+        this.toasterService.success(response.message,"Başarılı")
+        this.dataLoaded=true
+        
+      }
+      ,responseError=>{
+       
+        if(responseError.error.ValidationErrors.length > 0) {
+         
+          this.toasterService.error(responseError.error,"Hata!")
+        }
+        
+      })
+    }
+     else {
+      this.toasterService.error("Lütfen tüm alanları doldurunuz","Dikkat!")
+    }
+  }
+}
