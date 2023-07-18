b2c338357d03860b79f6b27a0db0a3abdc63d124
TopLap <saeed.zakeri1364@gmail.com>
2023-07-18 17:02:53 +0330

    F

--- src/app/models/houseStandard.ts
+++
@@ -1,9 +0,0 @@
-export interface HouseStandard {
-    houseId?: number;
-    locationId: number;
-    ownerId: number;
-    houseName: string;
-    modelYear: number;
-    dailyPrice: number;
-    description: string;
-  }
