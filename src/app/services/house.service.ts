7cef5facd593589c5edeff0bd1d2ba7e0e89ee13
TopLap <saeed.zakeri1364@gmail.com>
2023-07-18 17:02:26 +0330

    F

--- src/app/services/house.service.ts
+++ src/app/services/house.service.ts
@@ -1,68 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ListResponseModel } from '../models/listResponseModel';
-import { House } from '../models/house';
-import { ResponseModel } from '../models/responseModel';
-import { SingleResponseModel } from '../models/singleResponseModel';
-import { DashboardHouses} from '../models/dashboard-house';
-import { HouseStandard } from '../models/houseStandard';
-
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ListResponseModel} from '../models/listResponseModel';
+import {House} from '../models/house';
+import {ResponseModel} from '../models/responseModel';
+import {SingleResponseModel} from '../models/singleResponseModel';
+import {DashboardHouses} from '../models/dashboard-house';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HouseService {
-  apiUrl="https://localhost:44388/api/";
-  constructor(private httpClient:HttpClient) { }
 
-  getHouses():Observable<ListResponseModel<House>>{
-    let newPath= this.apiUrl+"houses/gethousedetails"
-    return this.httpClient.get<ListResponseModel<House>>(newPath)
+  apiUrl = "http://localhost:3000/";
+
+  constructor(private httpClient: HttpClient) {
   }
 
-  getHouseById(houseId: number): Observable<SingleResponseModel<House>> {
-    let newPath= this.apiUrl+"houses/getbyıd?houseId="+houseId
-    return this.httpClient.get<SingleResponseModel<House>>(
-      newPath
-    );
+  getAll(): Observable<ListResponseModel<House>> {
+    return this.httpClient.get<ListResponseModel<House>>(this.apiUrl + "houses");
   }
 
-  addHouse(house:House):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl + "houses/add", house)
-  }
-  
-  updateHouse(houseStandard:HouseStandard):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl + "houses/update", houseStandard)
+  getById(id: number): Observable<SingleResponseModel<House>> {
+    return this.httpClient.get<SingleResponseModel<House>>(this.apiUrl + "houses/" + id);
   }
 
-  deletHouse(houseStandard:HouseStandard):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl + "houses/delete", houseStandard)
+  add(house: House): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.apiUrl + "houses", house);
   }
 
-  
-  getHousesByBrand(brandId:number):Observable<ListResponseModel<House>>{
-    let newPath= this.apiUrl+"houses/getbybrand?brandId="+brandId
-    return this.httpClient.get<ListResponseModel<House>>(newPath)
+  update(house: House): Observable<ResponseModel> {
+    return this.httpClient.put<ResponseModel>(this.apiUrl + "houses/" + house.id, house);
   }
-  getHousesByColor(colorId:number):Observable<ListResponseModel<House>>{
-    let newPath= this.apiUrl+"houses/getbycolor?colorId="+colorId
-    return this.httpClient.get<ListResponseModel<House>>(newPath)
+
+  delete(id: number): Observable<ResponseModel> {
+    return this.httpClient.delete<ResponseModel>(this.apiUrl + "houses/" + id);
   }
-  getHousesBySelect(brandId:number, colorId:number){
-    let newPath = this.apiUrl + "houses/getbyselected?brandId=" + brandId + "&colorId=" + colorId;
-    return this.httpClient
-      .get<ListResponseModel<House>>(newPath);
-  }
-  getHouseDetail(houseId:number){
-    let newPath = this.apiUrl + "houses/gethousedetail?houseId=" + houseId;
-    return this.httpClient
-      .get<ListResponseModel<House>>(newPath);
-  }
-  
-  getAllHouseDetail(){
-    let newPath = this.apiUrl + "houses/getallhousedetail"
-    return this.httpClient
-      .get<ListResponseModel<DashboardHouses>>(newPath);
-  }
+
+  // getHousesByBrand(brandId:number):Observable<ListResponseModel<House>>{
+  //   let newPath= this.apiUrl+"houses/getbybrand?brandId="+brandId
+  //   return this.httpClient.get<ListResponseModel<House>>(newPath)
+  // }
+  // getHousesByColor(colorId:number):Observable<ListResponseModel<House>>{
+  //   let newPath= this.apiUrl+"houses/getbycolor?colorId="+colorId
+  //   return this.httpClient.get<ListResponseModel<House>>(newPath)
+  // }
+  // getHousesBySelect(brandId:number, colorId:number){
+  //   let newPath = this.apiUrl + "houses/getbyselected?brandId=" + brandId + "&colorId=" + colorId;
+  //   return this.httpClient
+  //     .get<ListResponseModel<House>>(newPath);
+  // }
+  // getHouseDetail(houseId:number){
+  //   let newPath = this.apiUrl + "houses/gethousedetail?houseId=" + houseId;
+  //   return this.httpClient
+  //     .get<ListResponseModel<House>>(newPath);
+  // }
+  //
+  // getAllHouseDetail(){
+  //   let newPath = this.apiUrl + "houses/getallhousedetail"
+  //   return this.httpClient
+  //     .get<ListResponseModel<DashboardHouses>>(newPath);
+  // }
 }
