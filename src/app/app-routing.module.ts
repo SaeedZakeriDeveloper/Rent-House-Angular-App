d73297e5d89e5ed920127d655e3c7d6d5be31a6c
TopLap <saeed.zakeri1364@gmail.com>
2023-07-19 01:52:26 +0330

    REGISTER

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-import {HouseComponent} from "./components/house/house.component";
 import {AdminDashboardComponent} from "./components/admin-dashboard/admin-dashboard.component";
+import {HouseComponent} from "./components/house/house.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
