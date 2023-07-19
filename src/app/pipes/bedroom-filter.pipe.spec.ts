0127e4c42ed273308f18665a9263bf53554769f7
saeedzakeri1364 <saeed.zakeri1364@gmail.com>
2023-07-19 14:23:40 -0400

    filter2

---
+++ src/app/pipes/bedroom-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BedroomFilterPipe } from './bedroom-filter.pipe';
+
+describe('BedroomFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BedroomFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/bedroom-filter.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Bedroom } from '../models/bedroom';
+
+@Pipe({
+  name: 'bedroomFilter'
+})
+export class BedroomFilterPipe implements PipeTransform {
+
+  transform(value: Bedroom[],bedroomFilter:string): Bedroom[]{
+  bedroomFilter= bedroomFilter?bedroomFilter.toLocaleLowerCase():""
+  return bedroomFilter?value.filter((c:Bedroom)=>c.title.toLocaleLowerCase().indexOf(bedroomFilter)!==-1):value  }
+}
